3752dbd4a0fec19aa704f56df98d58694ec8f99e
DGNuwan <dgnuwanthilakarathna@gmail.com>
2026-01-08 20:09:19 +0530

    update intro page

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoes_app/pages/home_page.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -11,6 +12,7 @@ class IntroPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 25.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
           
               //logo
@@ -56,7 +58,7 @@ class IntroPage extends StatelessWidget {
                   padding:const  EdgeInsets.all(25),
                   
                   child:const Center(
-                    child: const  Text('Shop Now',
+                    child: const Text('Shop Now',
                     style: TextStyle(color: Colors.white,
                     fontWeight: FontWeight.bold,
                     fontSize: 16,
